aac6e6262fcf2bc52c945c6a7316fb38341760b3
Petryk Roman <romic.argument@gmail.com>
2021-07-11 16:50:34 +0300

    Update fetch-api.js

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,10 +1,16 @@
-import counryCard from '../templates/country-card'
+import countryCard from '../templates/country-card'
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+defaultModules.set(PNotifyMobile, {});
 
 const rfs = {
     searchCountry: document.querySelector('.js-input-country'),
     countryCard: document.querySelector('.js-card-county'),
 }
-let numberOfCharactersInput = 0;
+
 var debounce = require('lodash.debounce');
 
 rfs.searchCountry.addEventListener('input', debounce(onInputChange, 3000));
@@ -12,14 +18,10 @@ rfs.searchCountry.addEventListener('input', debounce(onInputChange, 3000));
 function onInputChange(event) {
 
     const request = event.target.value;
-    console.log(numberOfCharactersInput += 1)
-    console.log(request)
-
-    // const searchQuery = request.elements.query.value;
 
     fetchCountry(request)
-    .then(renderCountryCard)
-    .catch(eve);
+        .then(renderCountryCard)
+        .catch(onFetchError)
 }
 
 function fetchCountry(countryName){
@@ -30,13 +32,13 @@ function fetchCountry(countryName){
 } 
 
 function renderCountryCard(country) {
-    rfs.countryCard.innerHTML = counryCard(...country);
+    rfs.countryCard.innerHTML = countryCard(...country);
 }
 
-function eve() {
+function onFetchError() {
     alert({
     type: 'error',
-    text: 'Notice me, senpai!',
-    delay: 3000
-});
+    text: 'We could not find a country that matched your query. Please try to enter the correct name',
+    delay: 3000}
+);
 }
