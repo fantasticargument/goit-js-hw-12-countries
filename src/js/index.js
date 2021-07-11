cee019785e505129f6ea50df144e78bc48f9d746
Petryk Roman <romic.argument@gmail.com>
2021-07-11 23:39:56 +0300

    Добавляє розмітку списку країн

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,51 @@
-import  './fetch-api';
+import countryCard from '../templates/country-card';
+import countryList from '../templates/country-list';
+import API from '../js/fetchCountries' ;
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 import { alert, defaultModules } from '@pnotify/core';
 import * as PNotifyMobile from '@pnotify/mobile';
-
 defaultModules.set(PNotifyMobile, {});
+
+const rfs = {
+    searchCountry: document.querySelector('.js-input-country'),
+    countryCard: document.querySelector('.js-card-county'),
+}
+
 var debounce = require('lodash.debounce');
 
+rfs.searchCountry.addEventListener('input', debounce(onInputChange, 3000));
 
+function onInputChange(event) {
+    const request = event.target.value;
 
+    API.fetchCountry(request)
+        .then(renderCountryCard)
+        .catch(onFetchError)
+        .finally(onFinallyClear)
+}
+
+function renderCountryCard(country) {
+    console.log(country);
+    if (country.length > 10) {
+        console.log('sg')
+    } else if (country.length > 1 && country.length < 10 ) {
+        rfs.countryCard.innerHTML = countryList(country);
+    } else {
+        rfs.countryCard.innerHTML = countryCard(...country);
+
+    }
+}
+
+function onFetchError() {
+    alert({
+    type: 'error',
+    text: 'We could not find a country that matched your query. Please try to enter the correct name',
+    delay: 3000}
+);
+}
+
+function onFinallyClear() {
+    rfs.searchCountry.value= ''
+}
